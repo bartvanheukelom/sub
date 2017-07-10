88e25815a607b010438d647af42798f0ad6cc4bc
Bart van Heukelom <mail@bartvh.nl>
2017-07-10 11:54:25 +0200

    main script pythonic refactoring

--- sub.py
+++ sub.py
@@ -7,18 +7,20 @@
 #
 
 import sys
+if __name__ == '__main__': 
+	SCRIPT_PYVER_REQUIRED = 3,5
+	if sys.version_info < SCRIPT_PYVER_REQUIRED:
+		print(SCRIPT_FILENAME, 'requires Python >=', SCRIPT_PYVER_REQUIRED, file=sys.stderr)
+		sys.exit(64)
+
 import svnwrap
 
 # submodules
 import ilog
 import icommit
 import browse
-
-if sys.version_info < (3,5):
-	print('Python >= 3.5 required')
-	exit(1)
 	
-def main():
+def main(argv):
 	
 	# check svn presence and version
 	version = svnwrap.run('--version','--quiet', output=svnwrap.OUT_TXT).replace("\n","");
@@ -26,87 +28,106 @@ def main():
 		print("WARNING - svn version is " + version + ", but sub was tested with 1.9")
 		
 	# run the given command
-	cmd(sys.argv[1] if len(sys.argv) >= 2 else "help", False, sys.argv[2:])
+	cmd(resolve_aliases((argv[1:]+['help'])[0]))(argv[2:])
 	
-# command shorthands
-def normalizeCmd(c):
-	if c == "gd":
-		return "gdiff"
-	elif (c == "ici"
-		or c == "ist" 
-		or c == "istatus"):
-		return "icommit"
-	elif (c == "ls"):
-		return "browse"
-	return c
-	
-def cmd(c, help, args):
-	
-	# resolve command name
+def cmd(c):
 	try:
-		fun = getattr(sys.modules[__name__], "cmd_" + normalizeCmd(c))
+		return getattr(commands, c)
 	except AttributeError:
 		print("Unknown command: " + c + ". Run `sub help` for help.")
 		exit(1)
-	
-	# invoke it
-	fun(help, args)
 
-# ------------ COMMANDS ------------ #
-	
-def cmd_help(help, args):
-	if help:
-		print("help:")
+aliases = {}
+aliases_reverse = {}
+def alias(*alts):
+	def aliased(f):
+		aliases_reverse[f.__name__] = alts
+		for a in alts:
+			aliases[a] = f.__name__
+		return f
+	return aliased
+
+def resolve_aliases(c):
+	while c in aliases:
+		c = aliases[c]
+	return c
+
+shorthelps = {}
+def shorthelp(s):
+	def ss(f):
+		shorthelps[f.__name__] = s
+		return f
+	return ss
+
+def fullshorthelp(c):
+	h = c
+	if c in aliases_reverse:
+		h += ' (' + ','.join(aliases_reverse[c]) + ')'
+	h += ': ' + shorthelps[c]
+	return h
+
+class commands:
+
+	def help_help():
 		print("usage:")
 		print("  1. help")
 		print("     Print the general help")
 		print("  2. help COMMAND")
 		print("     Print the help for the given COMMAND")
-	else:
-		if len(args) == 0:
-			print("Sub - utility commands for Subversion")
-			print("Commands: help, gdiff (gd), up, ilog, icommit (ici, istatus, ist), browse (ls)")
-			print("Run `help COMMAND` to get help for one of these.")
+	@shorthelp('Get some help')
+	def help(args):
+		if args:
+			c = resolve_aliases(args[0])
+			print(fullshorthelp(c))
+			cmd('help_' + c)()
 		else:
-			cmd(args[0], True, None)
-			
-def cmd_gdiff(help, args):
-	if help:
-		print("gdiff (gd): Graphical diff")
+			print("Sub - utility commands for Subversion")
+			print("Commands:")
+			print('   ', 'COMMAND'.ljust(8), 'ALTS'.ljust(16), 'INFO')
+			for c in shorthelps:
+				alts = ','.join(aliases_reverse.get(c, []))
+				print('   ', c.ljust(8), alts.ljust(16), shorthelps[c])
+			print("Run `help COMMAND` to get help for one of these.")
+				
+
+	def help_gdiff():
 		print("usage:")
 		print("  1. gdiff [OPTIONS...]")
 		print("     Shorthand for `svn diff --diff-cmd meld --git $OPTIONS`")
-	else:
-		cmd = ['diff','--diff-cmd','meld','--git']
-		cmd.extend(args)
-		svnwrap.run(*cmd)
-	
-def cmd_up(help, args):
-	if help:
-		print("TODO HELP")
-	else:
-		cmd = ['up']
-		cmd.extend(args)
-		svnwrap.run(*cmd)
+	@alias('gd')
+	@shorthelp('Graphical diff')
+	def gdiff(args):
+		svnwrap.run(*(['diff','--diff-cmd','meld','--git']+args))
 		
-def cmd_ilog(help, args):
-	if help:
-		print("ilog: Interactive log")
-	else:
+
+	def help_up():
+		pass
+	@shorthelp('Update')
+	def up(args):
+		svnwrap.run(*(['up']+args))
+			
+
+	def help_ilog():
+		pass
+	@shorthelp('Interactive log')
+	def ilog(args):
 		ilog.start(args)
-		
-def cmd_icommit(help, args):
-	if help:
-		print("icommit (ici): Interactive commit dialog")
-	else:
+			
+
+	def help_icommit():
+		pass
+	@alias('ici', 'istatus', 'ist')
+	@shorthelp('Interactive status and commit screen')
+	def icommit(args):
 		icommit.start(args)
 
-def cmd_browse(help, args):
-	if help:
-		print("browse: Repo browser")
-	else:
-		browse.start(args)
-		
-# ---- END OF COMMANDS ---- #
 
-main()
+	def help_browse():
+		pass
+	@alias('ls')
+	@shorthelp('Repo browser')
+	def browse(args):
+		browse.start(args)
+			
+
+if __name__ == '__main__': main(sys.argv)
